a19d887099b56a9a6404069fed1ffba8753d557d
SUDHANSHU GHOSH <shudhanshukumar9713@gmail.com>
2025-05-12 17:30:35 +0530

    Add chatapp dependency and update production configurations
    - Added "chatapp" as a dependency in both backend and frontend package.json files.
    - Updated backend index.js to serve frontend files in production.
    - Changed title in frontend index.html to "MitraMilap: Smooth chat".
    - Modified axios instance and useAuthStore to use environment-based base URLs.

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
+import path from "path";
+
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.routes.js"
 import messageRoutes from "./routes/message.routes.js";
@@ -23,9 +25,19 @@ app.use(
 );
 
 const port = process.env.PORT
+const __dirname = path.resolve();
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
 server.listen(port, () => {
     console.log(`server is running on port: ${port}`);
     console.log(`http://localhost:${port}`); // Log the URL to the console
